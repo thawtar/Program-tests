8e7f252e01059eeb29d8f29dc03b4d6b2f63f86e
thawtar <mr.thaw.tar1990@gmail.com>
2022-08-02 11:05:49 +0900

    Update helloworld.py

--- tk_test/helloworld.py
+++ tk_test/helloworld.py
@@ -2,3 +2,20 @@ from tkinter import *
 
 root = Tk()
 
+e = Entry(root)
+e.pack()
+
+def myClick():
+    hello = "Hello "+e.get()
+    myLabel = Label(root,text=hello)
+    myLabel.pack()
+
+myLabel = Label(root, text="Hello World!")
+
+myButton = Button(root,text="Click me!",command=myClick)
+myButton.pack()
+
+myLabel.pack()
+
+root.mainloop()
+
